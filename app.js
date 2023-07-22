7a9fe1e4e912ca8b570ea0cd7bafcf9c949283c7
Lynxsole <john1880@outlook.fr>
2023-07-22 20:33:20 +0200

    new file:   .vscode/settings.json 	new file:   app.js = index.js rename 	modified:   modification de quelques elements 	new file:   ajout d'un fichier style responce 	modified:   ajustement des element de la page 	new file:   ajout d'un fichier de test

---
+++ app.js
@@ -0,0 +1,28 @@
+
+var input = document.getElementById("enterId").value; 
+const convert = new Intl.NumberFormat('eu-FR').format();
+
+function getId()
+{
+    var discord_key = input;
+    var xmlHttpRequest = new XMLHttpRequest();
+    xmlHttpRequest.open("GET", "https://discord.com/api/guilds/" + discord_key + "/embed.json", true);
+    xmlHttpRequest.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var d = JSON.parse(this.responseText);
+            document.getElementById("mc").innerHTML = convert(d.presence_count);
+            document.getElementById('Warnig').style.color = "#1DC645";
+            document.getElementById('Warnig').innerHTML = "Voila !";
+        } 
+        else
+        {
+            document.getElementById('Warnig').style.color = "#C40227";
+            document.getElementById('mc').style.color = "#C40227";
+            document.getElementById('Warnig').innerHTML = "Une erreur C'est produit";
+            document.getElementById('mc').innerHTML = "erreur";
+        }
+    };  
+
+
+    xmlHttpRequest.send();
+}
--- index.js
+++
@@ -1,9 +0,0 @@
-let Membercounter = 13008;
-let Member = document.getElementById('mc').innerHTML = Membercounter;
-const convert = new Intl.NumberFormat('eu-FR').format(Membercounter)
-
-
-if (Membercounter > 1000) {
-    
-    document.getElementById('mc').innerHTML = convert;
-}
